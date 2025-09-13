9a97c53a74b70d201e905201feeb9d888d40dc49
Troy Vitullo <tvitullo@cprmail.org>
2025-09-13 16:26:18 -0600

    Functionality for image library

--- post_level_alt.js
+++ post_level_alt.js
@@ -1,4 +1,5 @@
 function createModal(id) {
+	console.log('inside createModal we are looking for '+id);
     // Create the modal container
     var modal = document.createElement('div');
     modal.style.position = 'fixed';
@@ -20,13 +21,13 @@ function createModal(id) {
     modalContent.style.padding = '20px';
     modalContent.style.borderRadius = '5px';
     modalContent.style.position = 'relative';
-    modal.style.left = '350px';
+    modalContent.style.left = '350px';
 
     // Append the content container to the modal
     modal.appendChild(modalContent);
 
     // Append the modal to the body
-	console.log('id is '+id);
+	console.log('modalHolder is looking for '+id);
 	var modalHolder = document.getElementById(id);
 	modalHolder.appendChild(modal);
 
@@ -38,7 +39,7 @@ function createModal(id) {
     });
 
     // Generate and set the modal content
-    genModalContent();
+    genModalInner();
 
 	document.getElementById("acceptAltText").addEventListener('click', function(event) {
 		console.log('we are trying to run the alt text function');
@@ -63,14 +64,18 @@ async function fetchAltText(url) {
 }
 
 function setAltText(altText) {
-    const selectedBlock = wp.data.select('core/block-editor').getSelectedBlock();
-	 wp.data.dispatch('core/block-editor').updateBlockAttributes(selectedBlock.clientId, {
-        alt: altText
-    });
+	if(isMediaLibraryOpen()) {
+		document.getElementById('attachment-details-alt-text').value = altText;
+	} else {
+		const selectedBlock = wp.data.select('core/block-editor').getSelectedBlock();
+		 wp.data.dispatch('core/block-editor').updateBlockAttributes(selectedBlock.clientId, {
+			alt: altText
+		});
+	}
 }
 
 
-async function selectedImageMeta() {
+function selectedImageMeta() {
     /* Find the image src and the container it's in */
     const div = document.querySelector('.components-resizable-box__container.has-show-handle');
 
@@ -99,50 +104,57 @@ async function selectedImageMeta() {
     };
 }
 
-/*
-async function selectedImageMeta() {
-	// Find the image src and the container it's in 
-	const div = document.querySelector('.components-resizable-box__container.has-show-handle');
-	const imgSrc = div.querySelector('img').src;
-	const parentId = div.parentNode.id;
-	console.log(imgSrc);
-	console.log(parentId);
-	let ret = new Array();
-	ret['imgSrc'] = imgSrc;
-	ret['parentId'] = parentId;
-	// console.log(ret);
-	console.log(ret['imgSrc']);
-	console.log(ret['parentId']);
-	console.log('^^ before returning');
-	return ret;
+function featuredImageUrl() {
+	console.log('running featuredImageUrl');
+	let imgSrc = document.getElementById("attachment-details-copy-link").value;
+	console.log('returning '+imgSrc);
+	return imgSrc;
 }
-	*/
+
+function isMediaLibraryOpen() {
+    return !!document.querySelector('.media-modal');
+}
 
 async function fetchAltTexts() {
 	/* Base function. Fetch alt text from all edge providers */
-	console.log('no this is new');
+	console.log('at the top of the function');
+
+	let origImage;
+	let parent;
+
 	let llava         = 'https://alt-text-worker-llava.cprapps.workers.dev/?image_url=';
-	let origImageData = await selectedImageMeta();
-	if(origImageData.error) {
-		console.error(origImageData.error);
-		alert("I (insofar as I have a sense of self) can't find an image. Did you select one on the page?");
-	} else {
-		console.log(origImageData );
-		console.log(origImageData.imgSrc); 
-		console.log(origImageData.parentId); 
-		console.log('origImageData ^^');
-		let origImage = origImageData.imgSrc; 
-		let parent = origImageData.parentId; 
-		console.log("parent is "+parent+" and origImage is "+origImage);
+	let imagePrepend  = "https://www.cpr.org/cdn-cgi/image/width=1080,quality=75,format=auto/";
+
+	/* find out if we are on the post page or on the media library page */
+	if(isMediaLibraryOpen()) {
+		console.log('library is open');
+		// we are in the media library
+		origImage = featuredImageUrl();
+		console.log('we have origImage of '+origImage );
+		parent = 'media-frame-title'; // that's the element id our modal will attach to
 		createModal(parent);
-		let imageUrl      = "https://www.cpr.org/cdn-cgi/image/width=1080,quality=75,format=auto/"+origImage;
-		let llavaText     = await fetchAltText(llava+imageUrl);
-		document.getElementById('llava').value=llavaText;
-		document.getElementById('lvmLoadingMessage').innerHTML="Here's what the machine came up with!";
+		console.log("hey lou parent is "+parent+" and origImage is "+origImage);
+	} else {
+		console.log('library is closed');
+		let origImageData = selectedImageMeta();
+		if(origImageData.error) {
+			console.error(origImageData.error);
+			alert("I (insofar as I have a sense of self) can't find an image. Did you select one on the page?");
+		} else {
+			origImage = origImageData.imgSrc; 
+			parent = origImageData.parentId; 
+			createModal(parent);
+		}
 	}
+	console.log("Final origImage:", origImage);
+	let imageUrl      = imagePrepend+origImage;
+	console.log("sending "+imageUrl);
+	let llavaText     = await fetchAltText(llava+imageUrl);
+	document.getElementById('llava').value=llavaText;
+	document.getElementById('lvmLoadingMessage').innerHTML="Here's what the machine came up with!";
 }
 
-function genModalContent() {
+function genModalInner() {
 	var modalInner = '<h3 id="lvmLoadingMessage">Getting LVM results ... </h3>\
 	\
 	<table>\
